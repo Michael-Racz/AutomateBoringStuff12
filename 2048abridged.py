9d41bc980107b9cb75a8b3ff28079c4d658ff6e6
Michael Racz <39655509+Michael-Racz@users.noreply.github.com>
2020-07-21 20:18:21 -0500

    Update 2048abridged.py

--- 2048abridged.py
+++ 2048abridged.py
@@ -8,10 +8,14 @@ from pandas import DataFrame
 import matplotlib.pyplot as plt
 browser = webdriver.Firefox()
 browser.get('https://play2048.co/')
+experiment_dict ={}
 score_list =[]
-experiment_dict ={'Experiment #': [1,2,3,4,5,6,7,8,9,10]}
-
-
+experiment_list =[]
+NUM_GAMES = 10
+#make a range to match values in the score list
+for m in range(NUM_GAMES):
+   experiment_list.append(m+1)
+   
 keySend = browser.find_element_by_tag_name('html')
 #Used to tell if the game has shown the "GAME OVER" text
 gameOverText = browser.find_element_by_css_selector('.game-message > p:nth-child(1)')
@@ -22,7 +26,7 @@ scoreBox = browser.find_element_by_class_name('score-container')
 
 #TODO: Try different patterns or let the user pick from a set of patterns
 i = 0
-while True and i < 10:
+while True and i < NUM_GAMES:
 
    keySend.send_keys(Keys.UP)
    #time.sleep(.1)
@@ -43,7 +47,13 @@ while True and i < 10:
       score_list.append(int(scoreBox.text))
       newGameButton.click()
 
+#Making the scatter plot
 experiment_dict.setdefault('Score',score_list)
+experiment_dict.setdefault('Experiment #',experiment_list)
+#Make the frame for the plot
 df = DataFrame(experiment_dict, columns = ['Score','Experiment #'])
+#Make the data plot
 df.plot(x = 'Experiment #', y ='Score',kind = 'scatter')
+
+#Show 
 plt.show()
